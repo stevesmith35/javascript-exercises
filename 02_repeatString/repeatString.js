8e32e5a97e4884f999860e52b54e180bfb38e975
Steven Smith <smith1386@gmail.com>
2021-10-13 17:33:18 -0400

    Finished Exercises 1, 2, and 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(a='', num) {
+    if (num < 0){
+        return 'ERROR';
+    } else {
+        for (let i = 0; i <= 3; i++) {
+            return a.repeat(num);
+        }
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function(str) {
+const reverseString = function(str='') {
+    let arr = str.split("");
+    let arr_rev = [];
 
+    for (let i = 1; i <= str.length; i++) {
+        temp = arr.pop();
+        arr_rev.push(temp);
+    }
+
+    return arr_rev.join('');
 };
 
 // Do not edit below this line
