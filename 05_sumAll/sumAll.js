10f2c358f079d3095c66c390fe02dad96a3075ff
Steven Smith <smith1386@gmail.com>
2021-10-13 22:34:27 -0400

    Completed Exercises 04 and 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (a < 0 || !Number.isInteger(a) || !Number.isInteger(b) || b < 0) {
+        return 'ERROR';
+    }
+
+    if (typeof a === 'string' || typeof b === 'string') {
+        return 'ERROR'
+    }
+
+    let sum = 0
+    if (a < b) {
+        for (a; a <= b; a++) {
+            sum += a;
+        }
+    } else if (a > b){
+        for (b; b <= a; b++) {
+            sum += b;
+        }
+    } 
+    return sum;
 };
 
 // Do not edit below this line
