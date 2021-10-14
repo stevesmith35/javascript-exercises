8a3c5bf0061e35ade82b2160deb4a6c8e1e8f9a6
Steven Smith <smith1386@gmail.com>
2021-10-14 05:51:21 -0400

    Finished Exercises 06 and 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  let c = (f - 32) * (5/9);
 
+  if (Number.isInteger(c) ) {
+    return parseInt(c);
+  } else {
+    return parseFloat(c.toFixed(1));
+  }
 };
 
-const ctof = function() {
+const ctof = function(c) {
+  let f = (c * 1.8000 + 32);
 
+  if (Number.isInteger(f)){
+    return parseInt(f);
+  } else {
+    return parseFloat(f.toFixed(1));
+  }
 };
 
 // Do not edit below this line
